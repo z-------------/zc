d9e8a982ed98733c3e220b70b95c6440e6543b90
Zack Guard <zacharyguard@gmail.com>
2022-10-08 20:41:38 +0800

    Make test nicer

--- tests/test.nim
+++ tests/test.nim
@@ -5,33 +5,44 @@ import std/[
   math,
 ]
 
+template checkExprValue(expr: string; argument: float; expected: float): untyped =
+  let val = evaluate(expr, argument)
+  check val.isSome
+  if val.isSome:
+    check val.unsafeGet.almostEqual(expected)
+
+template checkExprValue(expr: string; expected: float): untyped =
+  checkExprValue(expr, 0.0, expected)
+
 test "it works":
-  check evaluate("""2.17 * sin(pi + sqrt(4))""").get.almostEqual(2.17 * sin(Pi + sqrt(4.0)))
+  checkExprValue "2.17 * sin(pi + sqrt(4))":
+    2.17 * sin(Pi + sqrt(4.0))
 
   const x = -50.5
-  check evaluate("""2.17 * tan(x + 3 * x / sin(x ^ 2))""", x).get.almostEqual(2.17 * tan(x + ((3 * x) / sin(x ^ 2))))
+  checkExprValue "2.17 * tan(x + 3 * x / sin(x ^ 2))", x:
+    2.17 * tan(x + ((3 * x) / sin(x ^ 2)))
 
 test "operations return correct values":
   let pi = Pi # black magic fuckery happens if you directly use Pi
 
-  check evaluate("""69 + 420""").get.almostEqual(69 + 420)
-  check evaluate("""69 - 420""").get.almostEqual(69 - 420)
-  check evaluate("""69 * 420""").get.almostEqual(69 * 420)
-  check evaluate("""69 / 420""").get.almostEqual(69 / 420)
-  check evaluate("""69 ^ 420""").get.almostEqual(69.0.pow(420.0))
-  check evaluate("""sin(pi * 1.5)""").get.almostEqual(sin(pi * 1.5))
-  check evaluate("""cos(pi * 1.5)""").get.almostEqual(cos(pi * 1.5))
-  check evaluate("""tan(pi * 1.5)""").get.almostEqual(tan(pi * 1.5))
-  check evaluate("""asin(0.69)""").get.almostEqual(arcsin(0.69))
-  check evaluate("""acos(0.69)""").get.almostEqual(arccos(0.69))
-  check evaluate("""atan(0.69)""").get.almostEqual(arctan(0.69))
-  check evaluate("""abs(69 - 420)""").get.almostEqual(abs(69 - 420))
-  check evaluate("""sqrt(420)""").get.almostEqual(sqrt(420.0))
+  checkExprValue "69 + 420", 69 + 420
+  checkExprValue "69 - 420", 69 - 420
+  checkExprValue "69 * 420", 69 * 420
+  checkExprValue "69 / 420", 69 / 420
+  checkExprValue "69 ^ 420", 69.0.pow(420.0)
+  checkExprValue "sin(pi * 1.5)", sin(pi * 1.5)
+  checkExprValue "cos(pi * 1.5)", cos(pi * 1.5)
+  checkExprValue "tan(pi * 1.5)", tan(pi * 1.5)
+  checkExprValue "asin(0.69)", arcsin(0.69)
+  checkExprValue "acos(0.69)", arccos(0.69)
+  checkExprValue "atan(0.69)", arctan(0.69)
+  checkExprValue "abs(69 - 420)", abs(69 - 420)
+  checkExprValue "sqrt(420)", sqrt(420.0)
 
 test "idents work":
   const x = -50.5
 
-  check evaluate("x", x).get.almostEqual(x)
-  check evaluate("e").get.almostEqual(math.E)
-  check evaluate("tau").get.almostEqual(math.Tau)
-  check evaluate("pi").get.almostEqual(math.Pi)
+  checkExprValue "x", x, x
+  checkExprValue "e", math.E
+  checkExprValue "tau", math.Tau
+  checkExprValue "pi", math.Pi
