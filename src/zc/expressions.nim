2a192ed10ca673b1ea31901c09f0da6ac0c78e1c
Zack Guard <zacharyguard@gmail.com>
2023-01-24 14:25:17 +0800

    Less function call

--- src/zc/expressions.nim
+++ src/zc/expressions.nim
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with zc.  If not, see <http://www.gnu.org/licenses/>.
 
+{.experimental: "overloadableEnums".}
+
 import ./nodes
 import ./tokenize
 import ./parse
@@ -22,38 +24,54 @@ import std/[
   math,
   options,
   strutils,
-  typetraits,
 ]
+from std/typetraits import genericParams, get
 
 export options
 
 type
-  Instruction = proc (stack: var seq[float])
+  Op = enum
+    # Call, Infix
+    Plus = "+"
+    Minus = "-"
+    Times = "*"
+    Divide = "/"
+    Power = "^"
+    Sin = "sin"
+    Cos = "cos"
+    Tan = "tan"
+    Asin = "asin"
+    Acos = "acos"
+    Atan = "atan"
+    Abs = "abs"
+    Sqrt = "sqrt"
+    # Ident
+    X = "x"
+    E = "e"
+    Tau = "tau"
+    Pi = "pi"
+    # Literal
+    Literal
+  Instruction = object
+    case op: Op
+    of Plus..Pi:
+      discard
+    of Literal:
+      value: float
   Expression* = distinct seq[Instruction]
 
 template none(): untyped =
   none(genericParams(typeof result).get(0))
 
-# instructions
-
-template uniFunc(expr: untyped): Instruction =
-  (func (stack: var seq[float]) =
-    let a = stack.pop
-    stack.add(expr(a))
-  )
-
-template biFunc(expr: untyped): Instruction =
-  (func (stack: var seq[float]) =
-    let
-      b {.inject.} = stack.pop
-      a {.inject.} = stack.pop
-    stack.add(expr)
-  )
-
-template pushVal(value: float): Instruction =
-  (func (stack: var seq[float]) =
-    stack.add(value)
-  )
+template parseEnum[T: enum](r: Slice[T]; s: string): Option[T] =
+  try:
+    let e = parseEnum[T](s)
+    if e in r:
+      e.some
+    else:
+      T.none
+  except ValueError:
+    T.none
 
 # compile
 
@@ -70,36 +88,19 @@ func compile(node: Node): Option[Expression] =
   case node.kind
   of Call, Infix:
     let funcName = node.value
-    instructions.add:
-      case funcName
-      of "+": biFunc(a + b)
-      of "-": biFunc(a - b)
-      of "*": biFunc(a * b)
-      of "/": biFunc(a / b)
-      of "^": biFunc(a.pow(b))
-      of "sin": uniFunc(sin)
-      of "cos": uniFunc(cos)
-      of "tan": uniFunc(tan)
-      of "asin": uniFunc(arcsin)
-      of "acos": uniFunc(arccos)
-      of "atan": uniFunc(arctan)
-      of "abs": uniFunc(abs)
-      of "sqrt": uniFunc(sqrt)
-      else:
-        return none()
+    if (let op = parseEnum(Plus..Sqrt, funcName); op.isSome):
+      instructions.add Instruction(op: op.unsafeGet)
+    else:
+      return none()
   of Ident:
     let ident = node.value
-    instructions.add:
-      case ident
-      of "x": (func (stack: var seq[float]) = stack.add(stack[0]))
-      of "e": pushVal(math.E)
-      of "tau": pushVal(math.Tau)
-      of "pi": pushVal(math.Pi)
-      else:
-        return none()
+    if (let op = parseEnum(X..Pi, ident); op.isSome):
+      instructions.add Instruction(op: op.unsafeGet)
+    else:
+      return none()
   of Literal:
     let value = parseFloat(node.value)
-    instructions.add(pushVal(value))
+    instructions.add Instruction(op: Literal, value: value)
 
   Expression(instructions).some
 
@@ -112,12 +113,42 @@ func compile*(expr: string): Option[Expression] =
 
 # evaluate
 
+template uniOp(body: untyped) =
+  stack.add(body(stack.pop))
+
+template biOp(body: untyped) =
+  let
+    b {.inject.} = stack.pop
+    a {.inject.} = stack.pop
+  stack.add(body)
+
 proc evaluate*(instructions: Expression; argument = 0.0): Option[float] =
   let instructions = seq[Instruction](instructions)
 
   var stack = @[argument]
   for instruction in instructions:
-    instruction(stack)
+    case instruction.op
+    # Call, Infix
+    of Plus: biOp(a + b)
+    of Minus: biOp(a - b)
+    of Times: biOp(a * b)
+    of Divide: biOp(a / b)
+    of Power: biOp(a.pow(b))
+    of Sin: uniOp(sin)
+    of Cos: uniOp(cos)
+    of Tan: uniOp(tan)
+    of Asin: uniOp(arcsin)
+    of Acos: uniOp(arccos)
+    of Atan: uniOp(arctan)
+    of Abs: uniOp(abs)
+    of Sqrt: uniOp(sqrt)
+    # Ident
+    of X: stack.add(stack[0])
+    of E: stack.add(math.E)
+    of Tau: stack.add(math.Tau)
+    of Pi: stack.add(math.Pi)
+    # Literal
+    of Literal: stack.add(instruction.value)
 
   if stack.len >= 1:
     stack.pop.some
